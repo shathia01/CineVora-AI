d2c96c20a71b95dd000f288e3e2e18e82ac9bf95
shathia01 <shathiakala01@gmail.com>
2026-09-09 00:55:26 +0800

    Delete run_windows.bat

--- run_windows.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if not exist .venv (
-  echo Creating virtual environment...
-  python -m venv .venv
-)
-call .venv\Scripts\activate
-python -m pip install -r requirements.txt
-streamlit run app.py
